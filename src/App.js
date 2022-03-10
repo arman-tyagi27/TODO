df9a37a6f75569e46d0ac4c7c0728b393ea8218a
Arman Tyagi <Arman.Tyagi@bhavnacorp.com>
2022-03-10 17:24:15 +0530

    First TODO Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import { Todos } from './components/Todos';
+import { Footer } from './components/Footer';
+import React, {useState} from 'react';
+import { AddTodo } from './components/AddTodo';
+
 
 function App() {
+  const onDelete = (todo) =>{
+    console.log("Deleted", todo);
+    setTodos(todos.filter((e)=>{ 
+      return e!==todo;
+    }))
+  }
+
+const addTodo =(title, desc) =>{
+  console.log("I am adding this todo", title,desc)
+  let sno = todos[todos.length-1].sno + 1;
+  const myTodo ={
+    sno: sno,
+    title: title,
+    desc: desc,
+  }
+  setTodos([...todos, myTodo]);
+  console.log(myTodo);
+
+}
+
+  const [todos,setTodos] = useState([
+      {
+        sno: 1,
+        title: "Go to the Market",
+        desc: "I need to go to the market"
+      },
+      {
+        sno: 2,
+        title: "Go to the Mall",
+        desc: "I need to go to the mall for shopping"
+      },
+      {
+        sno: 3,
+        title: "Go to the Delhi",
+        desc: "I need to go to the Delhi to see Red Ford and Jama masjid"
+      },
+    ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header title="ToDoList" />
+      <AddTodo addTodo={addTodo}/>
+      <Todos todos= {todos} onDelete={onDelete}/>
+      <Footer />
+    </>
   );
 }
 
---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const Footer = () => {
+    let footerStyle={
+        
+    }
+  return (
+    <div className='bg-dark text-light py-3' style={footerStyle}>
+        <p className='text-center'>
+        Copyright &copy; MyTodoList.com
+        </p>
+    </div>
+  )
+}
